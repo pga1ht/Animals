ad6e15fdb58387465b2910177f43ed4098fa9928
pga1ht <pga1ht@gmail.com>
2019-02-16 13:50:50 -0500

    add new line of code

--- Excercises1/src/excercises1/Excercises1.java
+++ Excercises1/src/excercises1/Excercises1.java
@@ -15,6 +15,8 @@ public class Excercises1 {
 		
 		System.out.println(num1 + " + " + num2 + " = " + (num1+num2));
 		
+		System.out.println(num1 + " + " + num1 + " = " + (num1+num1));
+		
 		System.out.println(num1 + " - " + num2 + " = " + (num1-num2));
 		
 		System.out.println(num1 + " X " + num2 + " = " + (num1*num2));
